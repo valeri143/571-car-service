947c0cc91327a30bc2bac85cbc63b2f845bb6a05
valeri143 <valeria23200523251605@gmail.com>
2023-10-09 20:02:40 +0300

    gearbox img

--- src/pages/CarServicePage/CarServiceGearboxRepairPage/CarServiceGearboxRepairPage.jsx
+++ src/pages/CarServicePage/CarServiceGearboxRepairPage/CarServiceGearboxRepairPage.jsx
@@ -1,20 +1,20 @@
 import { lazy } from 'react';
 import styled from 'styled-components';
-import bg1x from 'images/desktop/backgrounds/services/chassis-bg@1x-min.jpg';
-import bg2x from 'images/desktop/backgrounds/services/chassis-bg@2x-min.jpg';
+import bg1x from '../../../images/desktop/backgrounds/services/chassis-bg@1x-min.jpg';
+import bg2x from '../../../images/desktop/backgrounds/services/chassis-bg@2x-min.jpg';
 import { CarServiceRepairHero } from 'components/CarServiceRepair/CarServiceRepairHero';
 import { CarServiceGearboxRepairAbout } from 'components/CarServiceRepair/CarServiceGearboxRepairAbout';
 
 const SectionFeedbacks = lazy(() =>
-  import('components/SectionFeedbacks/SectionFeedbacks')
+  import('components/SectionFeedbacks/SectionFeedbacks'),
 );
 const SectionCertifications = lazy(() =>
-  import('components/SectionCertifications/SectionCertifications')
+  import('components/SectionCertifications/SectionCertifications'),
 );
 const SectionForm = lazy(() => import('components/SectionForm/SectionForm'));
 const SectionBlog = lazy(() => import('components/SectionBlog/SectionBlog'));
 const SectionContacts = lazy(() =>
-  import('components//SectionContacts/SectionContacts')
+  import('components//SectionContacts/SectionContacts'),
 );
 
 const StyledBackGroundImage = styled.div`

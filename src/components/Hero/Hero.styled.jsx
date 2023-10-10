dfe459c07c23741593ccfddd8dae7e30cb5c8602
valeri143 <valeria23200523251605@gmail.com>
2023-10-10 12:13:14 +0300

    styled container

--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -46,7 +46,7 @@ export const StyledContainerHero = styled.div`
     width: 768px;
   }
   @media screen and (min-width: 1512px) {
-    width: 100vw;
+    width: 99vw;
     padding: 100px 156px 150px 156px;
   }
 `;
